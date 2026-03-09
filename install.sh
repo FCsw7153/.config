94acfc653be71d4bc78bc3a5b504fce0add90bec
fcsw <fc1694007153@gmail.com>
2026-03-09 21:44:11 +0800

    add: add tmux conf

--- install.sh
+++ install.sh
@@ -17,5 +17,6 @@ link() {
 link "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
 link "$DOTFILES/zsh/zimrc" "$HOME/.zimrc"
 link "$DOTFILES/git/.gitconfig" "$HOME/.gitconfig"
+link "$DOTFILES/tmux/tmux.conf" "$HOME/.tmux.conf"
 
 echo "Done!"
