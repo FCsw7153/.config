46e3590f16930def623175fb700d9034fc448d4c
fcsw <fc1694007153@gmail.com>
2026-03-09 21:29:14 +0800

    fixing: nvim add clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,19 @@ require("core.lazy")
 
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = function()
+      return { vim.fn.getreg(""), vim.fn.getregtype("") }
+    end,
+    ["*"] = function()
+      return { vim.fn.getreg(""), vim.fn.getregtype("") }
+    end,
+  },
+}
