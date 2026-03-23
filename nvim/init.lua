427eeb5a30be69542f547ae4cd1d00f8e543a979
fcsw <fc1694007153@gmail.com>
2026-03-23 15:42:23 +0800

    update tmux and nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,39 @@
 require("core.basic")
-require("core.keymap") 
-require("core.lazy")   
-
+require("core.keymap")
+require("core.lazy")
 
 vim.opt.clipboard = "unnamedplus"
 
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = function()
-      return { vim.fn.getreg(""), vim.fn.getregtype("") }
-    end,
-    ["*"] = function()
-      return { vim.fn.getreg(""), vim.fn.getregtype("") }
-    end,
-  },
-}
+if vim.fn.has("macunix") == 1 then
+  vim.g.clipboard = {
+    name = "macOS",
+    copy = {
+      ["+"] = "pbcopy",
+      ["*"] = "pbcopy",
+    },
+    paste = {
+      ["+"] = "pbpaste",
+      ["*"] = "pbpaste",
+    },
+    cache_enabled = 0,
+  }
+else
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = function()
+        return { vim.fn.getreg("+"), vim.fn.getregtype("+") }
+      end,
+      ["*"] = function()
+        return { vim.fn.getreg("*"), vim.fn.getregtype("*") }
+      end,
+    },
+    cache_enabled = 0,
+  }
+end
