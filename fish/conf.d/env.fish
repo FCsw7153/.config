757189e72888f971fba93fb762cd5a8bb023f60a
fcsw <fc1694007153@gmail.com>
2026-04-27 19:28:37 +0800

    conda env update

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,23 +1,40 @@
-# ===== conda =====
-if status is-interactive
-    if not functions -q conda
-        if set -q CONDA_EXE; and test -x "$CONDA_EXE"
-            "$CONDA_EXE" shell.fish hook | source
-        else if test -x "$HOME/anaconda3/bin/conda"
-            "$HOME/anaconda3/bin/conda" shell.fish hook | source
-        else if test -x "$HOME/miniconda3/bin/conda"
-            "$HOME/miniconda3/bin/conda" shell.fish hook | source
-        else if test -x /root/anaconda3/bin/conda
-            /root/anaconda3/bin/conda shell.fish hook | source
-        else if test -x /root/miniconda3/bin/conda
-            /root/miniconda3/bin/conda shell.fish hook | source
-        else if command -sq conda
-            conda shell.fish hook | source
-        end
+# ===== homebrew =====
+set -l brew_paths \
+    /opt/homebrew/bin/brew \
+    /usr/local/bin/brew \
+    /home/linuxbrew/.linuxbrew/bin/brew
+
+for brew_bin in $brew_paths
+    if test -x "$brew_bin"
+        "$brew_bin" shellenv fish | source
+        break
     end
 end
 
-# homebrew
-if test -x /home/linuxbrew/.linuxbrew/bin/brew
-    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# ===== conda =====
+if status is-interactive
+    if not functions -q conda
+        set -l conda_paths \
+            "$HOME/anaconda3/bin/conda" \
+            "$HOME/miniconda3/bin/conda" \
+            "/home/user/anaconda3/bin/conda" \
+            "/home/user/miniconda3/bin/conda" \
+            "/opt/conda/bin/conda" \
+            "/data/ml/anaconda3/bin/conda" \
+            "/data/ml/miniconda3/bin/conda" \
+            "/root/anaconda3/bin/conda" \
+            "/root/miniconda3/bin/conda"
+
+        if set -q CONDA_EXE; and test -x "$CONDA_EXE"
+            "$CONDA_EXE" shell.fish hook | source
+        else
+            for conda_bin in $conda_paths
+                if test -x "$conda_bin"
+                    "$conda_bin" shell.fish hook | source
+                    break
+                end
+            end
+        end
+    end
 end
